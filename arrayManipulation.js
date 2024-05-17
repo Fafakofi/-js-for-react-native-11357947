f2e6b2b8407c06ade10714d21b5e24b5b62d2c64
fafakofi <sfkbenson@st,ug.edu.gh>
2024-05-17 12:55:37 +0000

    task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -4,3 +4,11 @@ function processArray(bes) {
 }
 // example work
 // console.log(processArray([5,4,4,2])); 
+
+function formatArrayStrings(arrStr, arrnum){
+    return arrStr.map((a,b)=>{
+        return arrnum[b] % 2 === 0 ? a.toUpperCase() : a.toLowerCase();
+    })
+};
+
+// console.log(formatArrayStrings(["srbfi","srbrADSD"],[1,2]))
