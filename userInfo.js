8c609718fb274c20c889c6b5234645af6e06b916
fafakofi <sfkbenson@st,ug.edu.gh>
2024-05-19 17:57:32 +0000

    task 3

--- userInfo.js
+++ userInfo.js
@@ -1,16 +1,26 @@
-// const { processArray, formatArrayStrings } = require('./arrayManipulation');
 
-function createUserProfiles(originalNames, arrStr) {
+const { processArray, formatArrayStrings } = require('./arrayManipulation');
+
+function createUserProfiles(originalNames, modifiedNames) {
     return originalNames.map((name, index) => {
         return {
             id: index + 1,
             originalName: name,
-            arrStr:arrStr[index]
+            modifiedName: modifiedNames[index]
         };
     });
 }
 
-console.log(createUserProfiles(["cicc","uwnd"],["iwxxw","SUsxXX"]))
+// Example usage
+const numbers = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(numbers);
+
+const strings = ["Hello", "World", "JavaScript", "Is", "Awesome"];
+const modifiedNames = formatArrayStrings(strings, processedNumbers);
+
+const userProfiles = createUserProfiles(strings, modifiedNames);
+console.log(userProfiles);
+
 
 module.exports = {
     createUserProfiles
